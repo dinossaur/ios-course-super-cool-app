b8383d8b3f374adc08c141bfc96dd5e1154ba7b6
( ◡‿◡ ♡) <elasmosaurus@dinossaur.local>
2016-02-16 22:58:33 -0200

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by ( ◡‿◡ ♡) on 2/16/16.
+//  Copyright © 2016 Joyous Pizza Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
